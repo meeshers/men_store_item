319a623b8152ef835ebc0c50858466f49633d16e
Michelle <m.tran1223@gmail.com>
2020-05-26 15:54:27 -0700

    created index and show route

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 
 const PORT = 4000;
 
+app.set('view engine', 'ejs');
+// store
+
+// items
+
+// show index
+app.get('/', (req,res) =>{{
+  res.render('index');
+}})
+
+
 app.listen(PORT, function(){
   console.log(`listening on port ${PORT}`);
 })
